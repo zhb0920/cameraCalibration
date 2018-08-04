45a851f6cfc65baa5dfe52be0314d83787cce1f9
Zhanghongbin-github <1334340425@qq.com>
2018-08-04 09:08:56 +0800

    cameraCalibration

---
+++ build/CMakeFiles/cameracalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cameracalibration.dir/main.o"
+  "cameracalibration.pdb"
+  "cameracalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cameracalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
